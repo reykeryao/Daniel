f7f98fd9f6790cbf02bc68e9d2996231182a7ddb
Jun Yao <reykeryao@hotmail.com>
2025-09-26 14:23:16 -0500

    Clean up codes

--- JA25284/Step2_get_mapped_reads.sh
+++
@@ -1,2 +0,0 @@
-hisat2 -p 12 -x Ref/template -U Merged/$1.fq.gz 2> BAM/$1.log | samtools view -bS - > BAM/$1.bam
-samtools view -F16 BAM/$1.bam | cut -f 10 | sort | uniq -c | sort -k1,1nr | awk '{i++;print ">ID"i"_"$1"\n"$2}' > MAFFT/$1.fa
--- JA25284/cutadapt.sh
+++
@@ -1,6 +0,0 @@
-# $1 is the JA folder
-# $2 is the sample name
-cutadapt -O 3 -m 40 -j 24 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC \
-	-A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
-	-o Trim/$2.1.fq.gz -p Trim/$2.2.fq.gz $DATA/$1/$2.1.fq.gz $DATA/$1/$2.2.fq.gz 1>Trim/$2.log
-
